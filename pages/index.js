0ad3a147915ba2a5f5528c5adc4cdcb6774c64e4
AgtLucas <lucas@agtlucas.com>
2016-10-25 21:35:01 -0200

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import Head from 'next/head'
+import css from 'next/css'
 
 export default () => (
-  <div>Next!</div>
+  <div className={style}>
+    <Head>
+      <title>Home</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>Uprising!</p>
+  </div>
 )
+
+const style = css({
+  background: '#08C',
+  color: '#FFF',
+  ':hover': {
+    background: '#68C'
+  },
+  '@media (max-width: 600px)': {
+    background: '#000'
+  }
+})
